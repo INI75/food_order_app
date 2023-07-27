ceef6bac070f13b9aaeb0cfe0b7d2ff6dbd64d37
INI75 <isethugbosu@gmail.com>
2023-07-27 10:32:44 +0100

    fix tocken in progress

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,4 +1,25 @@
-Future<void> init() async{
+import 'package:food_order_app/controllers/popular_product_controller.dart';
+import 'package:food_order_app/data/api/api_client.dart';
+import 'package:food_order_app/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
 
-  
+Future<void> init() async {
+  /// API client
+  Get.lazyPut(
+    () => ApiClient(appBaseUrl: 'https://mvs.bslmeiyu.com'),
+  );
+
+//// Repo
+  Get.lazyPut(
+    () => PopularProductRepo(
+      apiClient: Get.find(),
+    ),
+  );
+
+  ////  //Controller
+  Get.lazyPut(
+    () => PopularProductController(
+      popularProductRepo: Get.find(),
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:food_order_app/Screens/Food/popular_food_details.dart';
 import 'package:food_order_app/Screens/Food/recommended_food_detail.dart';
 import 'package:food_order_app/Screens/home/main_food_page.dart';
+import 'package:food_order_app/controllers/popular_product_controller.dart';
+import 'package:food_order_app/helper/dependencies.dart' as dep;
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.find<PopularProductController>().getPopularProductList();
     return GetMaterialApp(
       title: 'Food Order App',
       debugShowCheckedModeBanner: false,
