de1615810a04b7a4bf2f29bfe635b24ba7697126
INI75 <isethugbosu@gmail.com>
2023-07-31 10:05:11 +0100

    apis start

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -14,10 +14,13 @@ class PopularProductController extends GetxController {
     Response response = await popularProductRepo.getPopularProductList();
 
     if (response.statusCode == 200) {
+      print('Me');
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products);
-
+      print(_popularProductList);
       update();
-    } else {}
+    } else {
+      print('network issues');
+    }
   }
 }
